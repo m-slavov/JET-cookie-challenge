a01725d40f86e408a2ce9f68e2744a6b18a5f2f6
m-slavov <mslavinovslavov@gmail.com>
2021-06-12 01:10:43 +0200

    - Added CookiesController & created GET method for retrieving all cookies and their ingredients
    - Added method to populate the database with cookies, ingredients and references between said cookies and ingredients

---
+++ JETCookies/JETCookies/Controllers/CookiesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JETCookies.Models;
+
+namespace JETCookies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CookiesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CookiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cookies
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var cookielist = _context.CookieItems.Select(o => new
+            {
+                o.Id,
+                o.Name,
+                o.Weight,
+                o.Calories,
+                Ingredient = o.CookieIngredients.Select(ot => ot.Ingredient).ToList()
+            }).ToList();
+
+            return Ok(cookielist);
+        }
+
+        private bool CookieExists(int id)
+        {
+            return _context.CookieItems.Any(e => e.Id == id);
+        }
+    }
+}
--- JETCookies/JETCookies/Models/SetUpDB.cs
+++ JETCookies/JETCookies/Models/SetUpDB.cs
@@ -21,6 +21,36 @@ namespace JETCookies.Models
         public static void AddData(DataContext cookiesDBContext)
         {
             cookiesDBContext.Database.Migrate();
+            if (!cookiesDBContext.CookieItems.Any())
+            {
+                cookiesDBContext.CookieItems.AddRange(
+
+                    new Cookie() { Name = "Chocolate Chip Cookies", Calories=488, Weight=100 }, //id 1
+                    new Cookie() { Name = "Shortbread Cookies", Calories=502, Weight=100 }, //id 2
+                    new Cookie() { Name= "Macaron Cookies", Calories=404, Weight=100 } //id 3
+
+                    );
+                cookiesDBContext.SaveChanges();
+
+                cookiesDBContext.IngredientItems.AddRange(
+                    
+                    new Ingredient() { Name="Dark chocolate" }, //id 1
+                    new Ingredient() { Name = "Sugar" }, //id 2
+                    new Ingredient() { Name = "Vanilla extract" }, //id 3
+                    new Ingredient() { Name ="Butter"} //id 4
+
+                    );
+                cookiesDBContext.SaveChanges();
+
+                cookiesDBContext.CookieIngredients.Add(new CookieIngredient() { CookieId = 1, IngredientId = 1 });
+                cookiesDBContext.CookieIngredients.Add(new CookieIngredient() { CookieId = 1, IngredientId = 2 });
+                cookiesDBContext.CookieIngredients.Add(new CookieIngredient() { CookieId = 2, IngredientId = 2 });
+                cookiesDBContext.CookieIngredients.Add(new CookieIngredient() { CookieId = 2, IngredientId = 4 });
+                cookiesDBContext.CookieIngredients.Add(new CookieIngredient() { CookieId = 3, IngredientId = 2 });
+                cookiesDBContext.CookieIngredients.Add(new CookieIngredient() { CookieId = 3, IngredientId = 3 });
+                cookiesDBContext.CookieIngredients.Add(new CookieIngredient() { CookieId = 3, IngredientId = 4 });
+                cookiesDBContext.SaveChanges();
+            }
         }
     }
 }
